a65f0d490cb18411126d9d6356ed119ddbcba1ac
Dalvik31 <10030167@itesa.edu.mx>
2021-09-03 13:59:40 -0500

    Use CustomSnackBar in MainActivity.kt

--- app/src/main/java/com/dalvik/customsnackbar/MainActivity.kt
+++ app/src/main/java/com/dalvik/customsnackbar/MainActivity.kt
@@ -2,10 +2,12 @@ package com.dalvik.customsnackbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dalvik.customtoast.CustomSnackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        CustomSnackbar.showSuccessToast(findViewById(R.id.container),"message")
     }
 }
